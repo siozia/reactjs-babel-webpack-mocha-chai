87f16b17acf82ebc352113cfa77ffe5c1b81b187
siozia <simone.iozia@gmail.com>
2017-09-29 19:35:40 +0200

    little hello world with start on dev mode

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports={
+    context:__dirname + '/src',
+    entry:"./index.js",
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    plugins: [       
+            new HtmlWebpackPlugin({
+                template: 'index.html',
+                inject: 'body'
+            }),
+        ]    
+};
